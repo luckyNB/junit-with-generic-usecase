beb506257e1e75f8784934a2925d3c87e2ab60ad
luckyNB <20150773@dbatu.ac.in>
2019-11-28 16:22:54 +0530

    Added:uc2:Finding max number number among three Float numbers

--- src/main/java/com/bridgelabz/MaxNumberFinder.java
+++ src/main/java/com/bridgelabz/MaxNumberFinder.java
@@ -11,4 +11,15 @@ public class MaxNumberFinder {
         }
         return max;
     }
+
+    public static Float findingMaxNumber(Float num1, Float num2, Float num3) {
+        Float max = num1;
+        if (num2.compareTo(max) > 0) {
+            max = num2;
+        }
+        if (num3.compareTo(max) > 0) {
+            max = num3;
+        }
+        return max;
+    }
 }
--- src/test/java/com/bridgelabz/JunitGenericsTestCases.java
+++ src/test/java/com/bridgelabz/JunitGenericsTestCases.java
@@ -6,8 +6,14 @@ import org.junit.Test;
 public class JunitGenericsTestCases {
 
     @Test
-    public void givenThreeNumber_Should_ReturnMaxAmoungThem() {
+    public void givenThreeNumber_AsInteger_Should_ReturnMaxAmoungThem() {
         Integer maxNumber = MaxNumberFinder.findingMaxNumber(20, 89, 45);
         Assert.assertEquals(Integer.valueOf(89), maxNumber);
+    }
+
+    @Test
+    public void givenThreeNumber_AsFloat_Should_ReturnMaxAmoungThem() {
+        Float maxNumber=MaxNumberFinder.findingMaxNumber(10.2f,45.3f,8.4f);
+        Assert.assertEquals(Float.valueOf(45.3f),maxNumber);
     }
 }
