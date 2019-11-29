7b53a106dc051db76ceda4ce24cda532cb0925c9
luckyNB <20150773@dbatu.ac.in>
2019-11-29 12:12:47 +0530

    Added:given  max number  at first location should return same max number

--- src/test/java/com/bridgelabz/JunitGenericsTestCases.java
+++ src/test/java/com/bridgelabz/JunitGenericsTestCases.java
@@ -16,4 +16,10 @@ public class JunitGenericsTestCases {
         Float maxNumber=MaxNumberFinder.findingMaxNumber(10.2f,45.3f,8.4f);
         Assert.assertEquals(Float.valueOf(45.3f),maxNumber);
     }
+
+    @Test
+    public void givenMaxNumberat_1stPosition_Should_ReturntheSameNumber() {
+        Integer  maxNumber=MaxNumberFinder.findingMaxNumber(30,20,10);
+        Assert.assertEquals(Integer.valueOf(30),maxNumber);
+    }
 }
